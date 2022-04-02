239c14f9540048e2588dd37ad2442f91379be230
MichalskiWiktor <wiktor.kasa1@gmail.com>
2022-04-02 14:19:21 +0200

    fixed no.ofelement problem and indexing out of bounds problem when creating new thread in the new visualiaztion

--- src/main/java/com/example/algorithmsvisualizer/BubbleSort.java
+++ src/main/java/com/example/algorithmsvisualizer/BubbleSort.java
@@ -5,15 +5,17 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
 public class BubbleSort extends Algorithm{
-    private float[] values;
+    private int[] values;
     private int length;
     private HBox hbox;
     private Line[] lines;
+    private double speed;
 
-    public BubbleSort(float arr[], HBox hbox){
-        this.values = arr;
-        this.length = this.values.length;
+    public BubbleSort(int values[], HBox hbox, double speed){
+        this.values = values;
         this.hbox = hbox;
+        this.speed = speed;
+        this.length = this.values.length;
         this.lines = new Line[this.length];
     }
     public void drawSortingVisualization() {
@@ -24,29 +26,33 @@ public class BubbleSort extends Algorithm{
                         if (values[j] > values[j+1]) {
                             int line1index = hbox.getChildren().indexOf(lines[j]);
                             int line2index = hbox.getChildren().indexOf(lines[j+1]);
-                            double line1endY = ((Line)hbox.getChildren().get(line1index)).getEndY();
-                            ((Line) hbox.getChildren().get(line1index)).setStroke(Color.WHITE);
-                            ((Line)hbox.getChildren().get(line1index)).setEndY(((Line)hbox.getChildren().get(line2index)).getEndY());
-                            ((Line)hbox.getChildren().get(line2index)).setEndY(line1endY);
-                            float temp = values[j];
+                            try{
+                                int line1endY = (int)((Line)hbox.getChildren().get(line1index)).getEndY();
+                                ((Line) hbox.getChildren().get(line1index)).setStroke(Color.WHITE);
+                                ((Line)hbox.getChildren().get(line1index)).setEndY(((Line)hbox.getChildren().get(line2index)).getEndY());
+                                ((Line)hbox.getChildren().get(line2index)).setEndY(line1endY);
+                            }catch(IndexOutOfBoundsException e){
+                                currentThread().stop();
+                            }
+                            int temp = values[j];
                             values[j] = values[j+1];
                             values[j+1] = temp;
                             try{
-                                Thread.sleep(100);
+                                Thread.sleep((long)(1000/speed));
                             }catch(InterruptedException e){
                                 e.printStackTrace();
-                                System.out.println("erro");
                             }
-                            ((Line) hbox.getChildren().get(line1index)).setStroke(Color.rgb(220, 156, 253));
+                            try{
+                                ((Line) hbox.getChildren().get(line1index)).setStroke(Color.rgb(220, 156, 253));
+                            }catch(IndexOutOfBoundsException e){
+                                currentThread().stop();
+                            }
                         }
                     }
                 }
             }
         };
         thread.start();
-        for(int i = 0; i < length-1; i++){
-            System.out.println(values[i]);
-        }
         ////after eending of visualizatyion add bigO notation and time it took to vinish algoritms vith picked speed
     }
     public void drawElements(){
--- src/main/java/com/example/algorithmsvisualizer/MainWindowController.java
+++ src/main/java/com/example/algorithmsvisualizer/MainWindowController.java
@@ -2,6 +2,7 @@ package com.example.algorithmsvisualizer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
@@ -10,39 +11,47 @@ import java.util.Random;
 public class MainWindowController{
     @FXML ComboBox algorithmPicker;
     @FXML TextField noOfElementsField;
+    @FXML Slider speedSlider;
     @FXML HBox hbox = new HBox();
     @FXML public void initialize(){
-        algorithmPicker.getItems().addAll("Bumble Sort", "Selection Sort", "Insertion Sort", "Quick Sort");
+        this.algorithmPicker.getItems().addAll("Bumble Sort", "Selection Sort", "Insertion Sort", "Quick Sort");
+        this.algorithmPicker.getSelectionModel().selectFirst();
     }
 
-    public void sort(){
+    public void createAlgorithm(){
         hbox.getChildren().clear();
-        int numElements = Integer.parseInt(this.noOfElementsField.getText());
-        if(numElements>0){
+        int numElements = 0;
+        /*Checking if user inputted correct numbers*/
+        try{
+            numElements = Integer.parseInt(this.noOfElementsField.getText());
+        }catch(NumberFormatException e){
+            System.out.println("It needs to be a number");
+        }
+        if(numElements>30){
+            numElements = 30;
+            this.noOfElementsField.setText("30");
+        }
+        if(numElements>=2){
             Algorithm algorithm;
             switch(this.algorithmPicker.getSelectionModel().getSelectedItem().toString()){
-                case "Bumble Sort":
-                    algorithm = new BubbleSort(getArrayWithRandomNumbers(numElements), this.hbox);
-                    break;
-                default:{
+                case "Bumble Sort" -> algorithm = new BubbleSort(getRandomNumbers(numElements), this.hbox, this.speedSlider.getValue());
+                default -> {
                     System.out.println("error");
                     algorithm = new Algorithm();
                 }
-
             }
             algorithm.drawElements();
             algorithm.drawSortingVisualization();
-        }
-        else{
-            System.out.println("error");
+        }else{
+            System.out.println("The number must be bigger than 1");
         }
     }
-    public float[] getArrayWithRandomNumbers(int len){
+    public int[] getRandomNumbers(int len){
         Random rand = new Random();
         int upperbound = 42;
-        float[] nums = new float[len];
+        int[] nums = new int[len];
         for(int i=0;i<len;i++){
-            nums[i] = rand.nextFloat(upperbound);
+            nums[i] = rand.nextInt(upperbound);
         }
         return nums;
     }
